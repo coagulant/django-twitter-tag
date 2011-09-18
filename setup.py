c4a5956954c1e42fe03956ef473397ce01555811
Ilya Baryshev <baryshev@gmail.com>
2011-09-19 00:17:57 +0400

    Added .gitignore Revamped setup.py to actually work with setuptools Version 0.1.0

--- setup.py
+++ setup.py
@@ -1,18 +1,40 @@
-from distutils.core import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+tests_require = [
+    'Django>=1.2,<1.4',
+    'mock==0.7.2'
+]
 
 setup(
     name='django-twitter-tag',
-    version='0.1.0dev',
+    version='0.1.0',
     author='Ilya Baryshev',
     author_email='baryshev@gmail.com',
-    packages=['twitter_tag'],
+    packages=find_packages(exclude=("tests")),
     url='http://github.com/futurecolors/django-template-tag/',
-    license='LICENSE.txt',
+    license='MIT',
     description='Shows last N tweets of a user in django template.',
     long_description=open('README.rst').read(),
+    dependency_links=[
+        'https://github.com/BonsaiDen/twitter-text-python/tarball/master#egg=twitter-text-python-1.0',
+    ],
     install_requires=[
         "django-templatetag-sugar==0.1",
-        "ttp==1.0",
-        "python-twitter==0.8.2"
+        "python-twitter==0.8.2",
+        "twitter-text-python>=1.0"
+    ],
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    classifiers = [
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
     ],
 )
