55e77a3583083a9f88321b86c4b2e0349d34403e
Ilya Baryshev <baryshev@gmail.com>
2011-09-18 23:04:20 +0400

    Added option to ignore retweets and/or replies Templatetag returns list of Status objects instad of dict Tests are rewritten to use Mock

--- twitter_tag/templatetags/twitter_tags.py
+++ twitter_tag/templatetags/twitter_tags.py
@@ -9,20 +9,21 @@ register = template.Library()
 
 
 @tag(register, [Constant("for"), Variable(), Constant("as"), Name(),
-                Optional([Constant("with"), Variable('replies')]),
-                Optional([Constant("limit"), Variable('limit')])])
-def get_tweets_new(context, username, asvar, replies=False, limit=None):
+                Optional([Constant("exclude"), Variable("exclude")]),
+                Optional([Constant("limit"), Variable("limit")])])
+def get_tweets_new(context, username, asvar, exclude='', limit=None):
+
     p = ttp.Parser()
     tweets = []
-    user_last_tweets = twitter.Api().GetUserTimeline(id=username, include_rts=True, include_entities=True)
+    include_rts = 'retweets' not in exclude
+    user_last_tweets = twitter.Api().GetUserTimeline(screen_name=username, include_rts=include_rts, include_entities=True)
 
     for status in user_last_tweets:
-        if not replies and status.GetInReplyToUserId() is not None:
+        if 'replies' in exclude and status.GetInReplyToUserId() is not None:
             continue
 
-        tweet = status.AsDict()
-        tweet['html'] = p.parse(tweet['text']).html
-        tweets.append(tweet)
+        status.html = p.parse(status.GetText()).html
+        tweets.append(status)
 
     if limit:
         tweets = tweets[:limit]
--- twitter_tag/tests.py
+++ twitter_tag/tests.py
@@ -1,37 +1,98 @@
+# -*- coding: utf-8 -*-
 from django.template import Template, Context
 from django.template.base import TemplateSyntaxError
 from django.test import TestCase
+from mock import patch
+import twitter
 
 
-class TwitterTagTestCase(TestCase):
+class StubGenerator(object):
+    TWEET_STUBS = {'jresig': [{'text': "This is not John Resig - you should be following @jeresig instead!",
+                               'html': "This is not John Resig - you should be following <a href=\"http://twitter.com/jeresig\">@jeresig</a> instead!"}],
+                   'futurecolors': [{'text': u"JetBrains радуют новыми фичами и апдейтами старых. Пост из блога #pycharm про дебаг шаблонов джанги в их IDE http://ht.ly/6viu3",},
+                                    {'text': u"На новых проектах будем использовать django-jenkins и django-any http://t.co/FjhHpdwV http://t.co/Hig8Hsjg Очень полезные штуки.",},
+                                    {'text': u"@goshakkk Переход на руби был связан именно с отсутствием поддержки py3k? :)",
+                                     'in_reply_to_user_id': 61236914},
+                                    {'text': u"Наконец-то начались какие-то попытки портировать #Django на #python3 http://t.co/XkftDsQH",
+                                     'retweeted': True},
+                                   ]
+                  }
+
+    @classmethod
+    def get_timeline(cls, screen_name, include_rts, **kwargs):
+        user = cls.get_user(screen_name=screen_name)
+        tweets = []
+        for stub in cls.TWEET_STUBS[screen_name]:
+            if not include_rts and stub.get('retweeted', False):
+                continue
+            html = stub.pop('html', '')
+            tweet = cls.get_status(user=user, **stub)
+            tweet.html = html
+            tweets.append(tweet)
+        return tweets
+
+    @classmethod
+    def get_user(cls, screen_name, **kwargs):
+        return twitter.User(screen_name=screen_name, **kwargs)
+
+    @classmethod
+    def get_status(cls, **kwargs):
+        return twitter.Status(**kwargs)
+
     
-    def test_twitter_tag_simple(self):
+class TwitterTagTestCase(TestCase):
+
+    def setUp(self):
+        patcher = patch('twitter.Api')
+        self.addCleanup(patcher.stop)
+        mock = patcher.start()
+        self.api = mock.return_value
+        self.api.GetUserTimeline.side_effect = StubGenerator.get_timeline
+
+
+    def test_twitter_tag_simple_mock(self):
         context = Context()
         template = Template( """{% load twitter_tags %}{% get_tweets_new for "jresig" as tweets %}""")
         template.render(context)
+        self.api.GetUserTimeline.assert_called_with(screen_name='jresig', include_rts=True, include_entities=True)
 
         self.assertEquals(len(context['tweets']), 1)
-        self.assertEquals(context['tweets'][0]['text'], 'This is not John Resig - you should be following @jeresig instead!')
-        self.assertEquals(context['tweets'][0]['html'], 'This is not John Resig - you should be following <a href="http://twitter.com/jeresig">@jeresig</a> instead!')
+        self.assertEquals(context['tweets'][0].text, 'This is not John Resig - you should be following @jeresig instead!')
+        self.assertEquals(context['tweets'][0].html, 'This is not John Resig - you should be following <a href="http://twitter.com/jeresig">@jeresig</a> instead!')
 
-
+    
     def test_twitter_tag_limit(self):
         context = Context()
         template = Template( """{% load twitter_tags %}{% get_tweets_new for "futurecolors" as tweets limit 2 %}""")
         template.render(context)
+        self.api.GetUserTimeline.assert_called_with(screen_name='futurecolors', include_rts=True, include_entities=True)
 
         self.assertEquals(len(context['tweets']), 2)
 
 
-    def test_twitter_tag_with_replies(self):
+    def test_twitter_tag_with_no_replies(self):
         context = Context()
-        template = Template( """{% load twitter_tags %}{% get_tweets_new for "futurecolors" as tweets with replies limit 2 %}""")
+        template = Template( """{% load twitter_tags %}{% get_tweets_new for "futurecolors" as tweets exclude "replies" limit 10 %}""")
         template.render(context)
+        self.api.GetUserTimeline.assert_called_with(screen_name='futurecolors', include_rts=True, include_entities=True)
 
-        self.assertEquals(len(context['tweets']), 2)
+        self.assertEquals(len(context['tweets']), 3)
+        self.assertEquals(context['tweets'][2].text, StubGenerator.TWEET_STUBS['futurecolors'][3]['text'])
 
-        
+
+    def test_twitter_tag_with_no_retweets(self):
+        context = Context()
+        template = Template( """{% load twitter_tags %}{% get_tweets_new for "futurecolors" as tweets exclude "retweets" %}""")
+        template.render(context)
+        self.api.GetUserTimeline.assert_called_with(screen_name='futurecolors', include_rts=False, include_entities=True)
+
+
+    def test_twitter_tag_with_no_replies_no_retweets(self):
+        context = Context()
+        template = Template( """{% load twitter_tags %}{% get_tweets_new for "futurecolors" as tweets exclude "retweets,replies" %}""")
+        template.render(context)
+        self.api.GetUserTimeline.assert_called_with(screen_name='futurecolors', include_rts=False, include_entities=True)
+
     def test_bad_syntax(self):
         self.assertRaises(TemplateSyntaxError, Template, """{% load twitter_tags %}{% get_tweets_new %}""")
-
-
+        self.assertRaises(TemplateSyntaxError, Template, """{% load twitter_tags %}{% get_tweets_new as "tweets" %}""")
