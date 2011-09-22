37ff5d59c652fb8f67a029aff7635f00a72e6594
Ilya Baryshev <baryshev@gmail.com>
2011-09-22 21:20:28 +0400

    Bumped version and updated package description

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ tests_require = [
 
 setup(
     name='django-twitter-tag',
-    version='0.1.0',
+    version='0.2.0',
     author='Ilya Baryshev',
     author_email='baryshev@gmail.com',
     packages=find_packages(exclude=("tests")),
     url='http://github.com/futurecolors/django-template-tag/',
     license='MIT',
-    description='Shows last N tweets of a user in django template.',
+    description="A django template tag to display user's recent tweets.",
     long_description=open('README.rst').read(),
     dependency_links=[
         'https://github.com/BonsaiDen/twitter-text-python/tarball/master#egg=twitter-text-python-1.0',
