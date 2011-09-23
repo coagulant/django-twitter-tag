fd93caf212c0bb6f1369cf5cfc45bb91dead1dbf
Ilya Baryshev <baryshev@gmail.com>
2011-09-24 00:08:37 +0400

    Added pylint validator to jenkins

--- tests/settings.py
+++ tests/settings.py
@@ -23,4 +23,5 @@ JENKINS_TASKS = (
     'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.django_tests',
     'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pylint',
 )
