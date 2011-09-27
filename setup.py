d7935feafebfa0131927ebd9f22b7fec2ad17849
Ilya Baryshev <baryshev@gmail.com>
2011-09-27 22:02:23 +0400

    Updated setup.py New github url (packange ot be moved) setup.py test imports refactor

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+from os import path
+import codecs
+from setuptools import setup, find_packages
+
+read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
 
 tests_require = [
     'Django>=1.2,<1.4',
@@ -17,10 +16,10 @@ setup(
     author='Ilya Baryshev',
     author_email='baryshev@gmail.com',
     packages=find_packages(exclude=("tests")),
-    url='https://github.com/futurecolors/django-twitter-tag',
+    url='https://github.com/coagulant/django-twitter-tag',
     license='MIT',
     description="A django template tag to display user's recent tweets.",
-    long_description=open('README.rst').read(),
+    long_description=read(path.join(path.dirname(__file__), 'README.rst')),
     dependency_links=[
         'https://github.com/BonsaiDen/twitter-text-python/tarball/master#egg=twitter-text-python-1.0',
     ],
@@ -30,8 +29,10 @@ setup(
         "twitter-text-python>=1.0"
     ],
     tests_require=tests_require,
+    test_suite = "runtests",
     extras_require={'test': tests_require},
     classifiers = [
+        'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
