060abeaf02bf53f1ed518029978846f6461a977d
Ilya Baryshev <baryshev@gmail.com>
2011-09-27 22:01:22 +0400

    Tests are now accessible via runtests.py

---
+++ runtests.py
@@ -0,0 +1,42 @@
+from django.core.management import execute_manager
+import sys
+from django.conf import settings
+
+PROJECT_APPS = ('twitter_tag', 'tests')
+
+try:
+    import django_jenkins
+    JENKINS_APP = ('django_jenkins',)
+except ImportError:
+    JENKINS_APP = tuple()
+
+settings.configure(DEBUG = True,
+                   DATABASE_ENGINE = 'sqlite3',
+                   DATABASES = {
+                        'default': {
+                            'ENGINE': 'django.db.backends.sqlite3',
+                            }
+                   },
+                   INSTALLED_APPS = ('django.contrib.auth',
+                                     'django.contrib.contenttypes',
+                                     'django.contrib.sessions',
+                                     'django.contrib.admin',) + PROJECT_APPS + JENKINS_APP,
+                   PROJECT_APPS = PROJECT_APPS,
+                   JENKINS_TASKS = (
+                        'django_jenkins.tasks.with_coverage',
+                        'django_jenkins.tasks.django_tests',
+                        'django_jenkins.tasks.run_pep8',
+                        'django_jenkins.tasks.run_pylint',
+                   ),
+                   ROOT_URLCONF = 'tests.urls'
+)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        sys.argv += ['test', 'tests']
+    execute_manager(settings)
+else:
+    from django.test.simple import DjangoTestSuiteRunner
+    failures = DjangoTestSuiteRunner().run_tests(['tests',])
+    if failures:
+        sys.exit(failures)
