d68ea9e8a96eadcbb088e16fb7fb19e5244e12a0
Ilya Baryshev <baryshev@gmail.com>
2012-03-31 23:09:26 +0400

    Bumped version to 0.4.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = [
 
 setup(
     name='django-twitter-tag',
-    version='0.4.0dev',
+    version='0.4.0',
     author='Ilya Baryshev',
     author_email='baryshev@gmail.com',
     packages=find_packages(exclude=("tests")),
