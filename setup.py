21a99b6c2b520a0ed1bd6dc9d35c69f8484680f1
coagulant <baryshev@gmail.com>
2013-02-10 02:05:44 +0400

    Bumped to 1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require = [
 
 setup(
     name='django-twitter-tag',
-    version='1.0dev',
+    version='1.0',
     author='Ilya Baryshev',
     author_email='baryshev@gmail.com',
     packages=find_packages(exclude="tests"),
