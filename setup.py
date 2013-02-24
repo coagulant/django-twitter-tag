bf27d19cc1b820bca7a995337e031826082bc878
coagulant <baryshev@gmail.com>
2013-02-24 15:05:54 +0400

    Bumping to 1.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require = [
 
 setup(
     name='django-twitter-tag',
-    version='1.1dev',
+    version='1.1',
     author='Ilya Baryshev',
     author_email='baryshev@gmail.com',
     packages=find_packages(exclude="tests"),
